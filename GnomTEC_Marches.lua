bc3b81a59bf6c6cb43965a0c4c3a8db0c1b7ed00
GnomTEC <info@gnomtec.de>
2017-08-30 19:24:05 +0200

    * Updated TOC for WoW patch 7.3.0

--- GnomTEC_Marches.lua
+++ GnomTEC_Marches.lua
@@ -1,6 +1,6 @@
 -- **********************************************************************
 -- GnomTEC Marches
--- Version: 7.2.0.1
+-- Version: 7.3.0.1
 -- Author: Peter Jack
 -- URL: http://www.gnomtec.de/
 -- **********************************************************************
@@ -28,14 +28,14 @@ local L = LibStub("AceLocale-3.0"):GetLocale("GnomTEC_Marches")
 local addonInfo = {
 	["Name"] = "GnomTEC Marches",
 	["Description"] = "GnomTEC Marches.",	
-	["Version"] = "7.2.0.1",
-	["Date"] = "2017-04-01",
+	["Version"] = "7.3.0.1",
+	["Date"] = "2017-08-30",
 	["Author"] = "Peter Jack",
 	["Email"] = "info@gnomtec.de",
 	["Website"] = "http://www.gnomtec.de/",
 	["Copyright"] = "© 2015-2107 by Peter Jack",
 	["License"] = "European Union Public Licence (EUPL v.1.1)",	
-	["FrameworkRevision"] = 9
+	["FrameworkRevision"] = 10
 }
 
 -- ----------------------------------------------------------------------
